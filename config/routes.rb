8560cc69f46f906afcb61ec18e37dd9f97a97600
Livia Lortscher <livialortscher@hotmail.com>
2019-02-13 17:15:09 +0100

    2 update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :new, :create, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+#   get 'restaurants', to: 'restaurants#index', as: :restaurants
+
+#   get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+#   post 'restaurants', to: 'restaurants#create'
+
+#   get 'restaurants/:id', to: 'restaurants#show', as: :show_restaurant
+# end
